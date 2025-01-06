ea389126a5b292084473a139bc93fb4c39d1b33d
Pouria-GH <pouria.ghafarzadeh@gmail.com>
2025-01-06 16:46:04 +0330

    Add intervat to Highlights row

--- js/script.js
+++ js/script.js
@@ -17,3 +17,35 @@ $(document).ready(() => {
         }
     });
 });
+// row 4
+let hlInter = setInterval(highlightsI,3000);
+let hlHover = [];
+let hlTabPane = $("#highlights .tab-pane");
+let hlListGroupI = $("#highlights .list-group a");
+for ( let i = 0; i < hlListGroupI.length; i++ ) {
+    hlHover.push(hlTabPane[i]);
+    hlHover.push(hlListGroupI[i]);
+}
+for ( let i = 0; i < hlHover.length; i++ ) {
+    hlHover[i].addEventListener("mouseover", function(){
+        clearInterval(hlInter);
+    });
+    hlHover[i].addEventListener("mouseout", function(){
+        hlInter = setInterval(highlightsI,3000);
+    });
+}
+
+let hlPointer = 0;
+function highlightsI() {
+    for ( let i = 0; i < hlListGroupI.length; i++ ) {
+        if( hlListGroupI[i] === document.activeElement ) {
+            hlPointer = i;
+        }
+    }
+    hlListGroupI.blur();
+    hlPointer++;
+    if(hlPointer == hlListGroupI.length) {
+        hlPointer = 0;
+    }
+    hlListGroupI[hlPointer].click();
+}
