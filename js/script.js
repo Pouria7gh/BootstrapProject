ed05043cd520c3378ef1617ac850b6b77a329dd6
Pouria-GH <pouria.ghafarzadeh@gmail.com>
2025-01-12 20:41:25 +0330

    Edit highlights row and add js to navbar

--- js/script.js
+++ js/script.js
@@ -18,34 +18,195 @@ $(document).ready(() => {
     });
 });
 // row 4
-let hlInter = setInterval(highlightsI,3000);
-let hlHover = [];
-let hlTabPane = $("#highlights .tab-pane");
-let hlListGroupI = $("#highlights .list-group a");
-for ( let i = 0; i < hlListGroupI.length; i++ ) {
-    hlHover.push(hlTabPane[i]);
-    hlHover.push(hlListGroupI[i]);
-}
-for ( let i = 0; i < hlHover.length; i++ ) {
-    hlHover[i].addEventListener("mouseover", function(){
-        clearInterval(hlInter);
-    });
-    hlHover[i].addEventListener("mouseout", function(){
-        hlInter = setInterval(highlightsI,3000);
-    });
-}
 
 let hlPointer = 0;
-function highlightsI() {
-    for ( let i = 0; i < hlListGroupI.length; i++ ) {
-        if( hlListGroupI[i] === document.activeElement ) {
+function hlInterval() {
+    $("#highlights .tab-content div").removeClass("active").removeClass("show");
+    $("#highlights .list-group a").removeClass("active");
+    for(let i = 0; i < $("#highlights .list-group a").length; i++) {
+        if($("#highlights .list-group a")[i] == document.activeElement) {
             hlPointer = i;
         }
     }
-    hlListGroupI.blur();
+    $("#highlights .list-group a").blur();
     hlPointer++;
-    if(hlPointer == hlListGroupI.length) {
+    if(hlPointer == $("#highlights .list-group a").length){
         hlPointer = 0;
     }
-    hlListGroupI[hlPointer].click();
+    $(`#highlights .tab-content div:nth-child(${hlPointer + 1})`).addClass("active").addClass("show");
+    $(`#highlights .list-group a:nth-child(${hlPointer + 1})`).addClass("active");
 }
+let hlTimer = setInterval(hlInterval, 3000);
+$("#highlights .tab-content div").mouseover(function(){
+    clearInterval(hlTimer);
+});
+$("#highlights .tab-content div").mouseout(function(){
+    hlTimer = setInterval(hlInterval, 3000);
+});
+
+$("#highlights .list-group a").mouseover(function(){
+    clearInterval(hlTimer);
+});
+$("#highlights .list-group a").mouseout(function(){
+    hlTimer = setInterval(hlInterval, 3000);
+});
+
+// navbar links scroll
+$(function(){
+   $("#homeLink").click(function(){
+    if(window.innerWidth < 992) {
+        $("nav button.navbar-toggler").click();
+    }
+    $("html, body").animate({
+        scrollTop: 0
+    }, 1000);
+   });
+
+   $("#learnMoreLink").click(function(){
+    if(window.innerWidth < 992) {
+        $("nav button.navbar-toggler").click();
+    }
+    $("html, body").animate({
+        scrollTop: ($("#learnMore").offset().top - 64.9534)
+    }, 1000);
+   });
+
+   $("#highlightsLink").click(function(){
+    if(window.innerWidth < 992) {
+        $("nav button.navbar-toggler").click();
+    }
+    $("html, body").animate({
+        scrollTop: ($("#highlights").offset().top - 64.9534)
+    }, 1000);
+   });
+
+   $("#ctaLink").click(function(){
+    if(window.innerWidth < 992) {
+        $("nav button.navbar-toggler").click();
+    }
+    $("html, body").animate({
+        scrollTop: ($("#cta").offset().top - 64.9534)
+    }, 1000);
+   });
+
+   $("#testiLink").click(function(){
+    if(window.innerWidth < 992) {
+        $("nav button.navbar-toggler").click();
+    }
+    $("html, body").animate({
+        scrollTop: ($("#testimonials").offset().top - 64.9534)
+    }, 1000);
+   });
+
+   $("#servicesLink").click(function(){
+    if(window.innerWidth < 992) {
+        $("nav button.navbar-toggler").click();
+    }
+    $("html, body").animate({
+        scrollTop: ($("#services").offset().top - 64.9534)
+    }, 1000);
+   });
+
+   $("#portfolioLink").click(function(){
+    if(window.innerWidth < 992) {
+        $("nav button.navbar-toggler").click();
+    }
+    $("html, body").animate({
+        scrollTop: ($("#portfolio").offset().top - 64.9534)
+    }, 1000);
+   });
+
+   $("#pricingLink").click(function(){
+    if(window.innerWidth < 992) {
+        $("nav button.navbar-toggler").click();
+    }
+    $("html, body").animate({
+        scrollTop: ($("#pricing").offset().top - 64.9534)
+    }, 1000);
+   });
+
+   $("#contactLink").click(function(){
+    if(window.innerWidth < 992) {
+        $("nav button.navbar-toggler").click();
+    }
+    $("html, body").animate({
+        scrollTop: ($("#contact").offset().top - 64.9534)
+    }, 1000);
+   });
+
+   $(document).on("scroll",function(){
+    let scroll = window.scrollY + 64.9534;
+
+    let homeS = $("#home").offset().top < scroll && ($("#home").offset().top + $("#home").innerHeight()) > scroll;
+
+    let learnMoreS = $("#learnMore").offset().top < scroll && ($("#learnMore").offset().top + $("#learnMore").innerHeight()) > scroll;
+    let highlightsS = $("#highlights").offset().top < scroll && ($("#highlights").offset().top + $("#highlights").innerHeight()) > scroll;
+    let ctaS = $("#cta").offset().top < scroll && ($("#cta").offset().top + $("#cta").innerHeight()) > scroll;
+    let testiS = $("#testimonials").offset().top < scroll && ($("#testimonials").offset().top + $("#testimonials").innerHeight()) > scroll;
+
+    let servicesS = $("#services").offset().top < scroll && ($("#services").offset().top + $("#services").innerHeight()) > scroll;
+    let portfolioS = $("#portfolio").offset().top < scroll && ($("#portfolio").offset().top + $("#portfolio").innerHeight()) > scroll;
+    let pricingS = $("#pricing").offset().top < scroll && ($("#pricing").offset().top + $("#pricing").innerHeight()) > scroll;
+    let contactS = $("#contact").offset().top < scroll && ($("#contact").offset().top + $("#contact").innerHeight()) > scroll;
+
+    if( homeS ) {
+        $("#homeLink").addClass("active");
+    } else {
+        $("#homeLink").removeClass("active");
+    }
+
+    if( learnMoreS ) {
+        $("#learnMoreLink").addClass("active");
+    } else {
+        $("#learnMoreLink").removeClass("active");
+    }
+
+    if( highlightsS ) {
+        $("#highlightsLink").addClass("active");
+    } else {
+        $("#highlightsLink").removeClass("active");
+    }
+
+    if( ctaS ) {
+        $("#ctaLink").addClass("active");
+    } else {
+        $("#ctaLink").removeClass("active");
+    }
+
+    if( testiS ) {
+        $("#testiLink").addClass("active");
+    } else {
+        $("#testiLink").removeClass("active");
+    }
+
+    if( servicesS ) {
+        $("#servicesLink").addClass("active");
+    } else {
+        $("#servicesLink").removeClass("active");
+    }
+
+    if( portfolioS ) {
+        $("#portfolioLink").addClass("active");
+    } else {
+        $("#portfolioLink").removeClass("active");
+    }
+
+    if( pricingS ) {
+        $("#pricingLink").addClass("active");
+    } else {
+        $("#pricingLink").removeClass("active");
+    }
+
+    if( contactS ) {
+        $("#contactLink").addClass("active");
+    } else {
+        $("#contactLink").removeClass("active");
+    }
+
+    if ( learnMoreS || highlightsS || ctaS || testiS ) {
+        $("#aboutLink").addClass("active");
+    } else {
+        $("#aboutLink").removeClass("active");
+    }
+   });
+});
